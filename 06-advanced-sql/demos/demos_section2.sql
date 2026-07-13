327161eafc9800204e4e3a60d76ee01dcc18f888
Alina Aleks <toalinaaleks@gmail.com>
2026-07-13 18:40:14 +0300

    Joining multiple tables

--- 06-advanced-sql/demos/demos_section2.sql
+++ 06-advanced-sql/demos/demos_section2.sql
@@ -69,8 +69,17 @@ FROM happiness_scores hs
     ON hs.country = ir.country_name AND hs.year = ir.year;
         
 -- 4. Joining multiple tables
+SELECT * FROM happiness_scores;
+SELECT * FROM country_stats;
+SELECT * FROM inflation_rates;
 
-
+SELECT	hs.year, hs.country, hs.happiness_score,
+		cs.continent, ir.inflation_rate
+FROM	happiness_scores hs
+		LEFT JOIN country_stats cs
+			ON hs.country = cs.country
+		LEFT JOIN inflation_rates ir
+			ON hs.year = ir.year AND hs.country = ir.country_name;
 
 -- 5. Self joins
 CREATE TABLE IF NOT EXISTS employees (
